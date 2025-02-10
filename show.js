32cbb97c43c357d5187c8b72c0a72660b8e62272
GreatMyles <mmcarpenter@wisc.edu>
2025-02-10 14:20:02 +0300

    built out home page a bit.

---
+++ show.js
@@ -0,0 +1,37 @@
+var more = document.querySelectorAll(".initiative-block .show-more");
+var less = document.querySelectorAll(".show-less");
+var hidden =  document.querySelectorAll(".hidden-text");
+var ellipses = document.querySelectorAll(".ellipses")
+
+
+function getIndex(arr, item) {
+  for (let i = 0; i < arr.length; i+=1) {
+    console.log(item); console.log(arr[i]);
+    if (arr[i] === item) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+more.forEach( (item) => {
+  item.addEventListener("click", function(evt) {
+    const idx = getIndex(more, this);
+    console.log(idx);
+    more[idx].style.display = "none";
+    less[idx].style.display = "block";
+    hidden[idx].style.display = "inline";
+    ellipses[idx].style.display ="none";
+  });
+});
+
+less.forEach( (item) => {
+  item.addEventListener("click", function(evt) {
+    const idx = getIndex(less, this);
+    more[idx].style.display = "block";
+    less[idx].style.display = "none";
+    hidden[idx].style.display = "none";
+    ellipses[idx].style.display = "inline";
+  });
+});
+
